bfccc05a8dc1d0294dbabfbb3f075e9262febd00
Alex Bajaña <alexb.inf.4891@gmail.com>
2023-11-06 14:13:29 -0500

    Extracción y presentación del modelo en tablas para exportar a latex

--- global.R
+++ global.R
@@ -63,3 +63,56 @@ coef_plot_plus <- function(tidy_tems,
 }
 
 
+# Función para poner los modelos en modo tabla ----------------------------
+
+extract_broom <- function(tidy_model, glance_model){
+  
+  # browser()
+  
+  # get estimates/standard errors from tidy
+  
+  coef <- tidy_model$estimate
+  
+  ci_low <- tidy_model$conf.low
+  ci_up <- tidy_model$conf.high
+  
+  coef.names <- as.character(tidy_model$term)
+  
+  se <- tidy_model$std.error
+  
+  pvalues <- tidy_model$p.value
+  
+  # get goodness-of-fit statistics from glance
+  
+  glance_transposed <- glance_model %>% 
+    select(-model) %>% 
+    pivot_longer(everything()) %>% 
+    filter(!is.na(value))
+  
+  # as_tibble(cbind(name = names(glance_model), value = t(glance_model)))
+  
+  gof.names <- as.character(glance_transposed$name)
+  
+  gof <- as.double(glance_transposed$value)
+  
+  gof.decimal <- gof %% 1 > 0
+  
+  # browser()
+  
+  tr_object <- texreg::createTexreg(coef.names = coef.names,
+                                    coef = coef,
+                                    se = se,
+                                    pvalues = pvalues,
+                                    gof.names = gof.names,
+                                    gof = gof,
+                                    gof.decimal = gof.decimal,
+                                    ci.low = ci_low,
+                                    ci.up = ci_up)
+  return(tr_object)
+}
+
+# extract_broom(tidy_model = dep_var_pff_p_joint_p %>% 
+#                 filter(model == "Event study saturated with fixed effects"),
+#               glance_model = dep_var_pff_p_joint %>% 
+#                 filter(model == "Event study saturated with fixed effects")) %>% 
+#   screenreg()
